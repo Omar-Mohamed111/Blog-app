93ddca373895aa14d1891fe3920b2ca273cce664
Omar Hamed <om893177@gmail.com>
2026-09-21 22:30:36 +0300

    Prepare backend for Vercel deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,23 @@
-require("dotenv").config()
+require("dotenv").config();
+
 const app = require("./app");
 const pool = require("./config/db");
-const PORT = process.env.PORT || 3000
 
+const PORT = process.env.PORT || 3000;
 
+// Local development
+if (require.main === module) {
+  app.listen(PORT, async () => {
+    try {
+      await pool.query("SELECT 1");
+      console.log(`Server Running In ${PORT} ✅`);
+      console.log("Database Connected ✅");
+    } catch (error) {
+      console.error("Database Connection Failed ❌");
+      console.error(error.message);
+    }
+  });
+}
 
-
-
-app.listen (PORT , async()=>{
-    // console.log(`Server Running In ${PORT} ✅`);
-    
-      try {
-    await pool.query("SELECT 1");
-    console.log(`Server Running In ${PORT} ✅`);
-    console.log("Database Connected ✅");
-  } catch (error) {
-    console.error("Database Connection Failed ❌");
-    console.error(error.message);
-  }
-
-})
+// Vercel
+module.exports = app;
